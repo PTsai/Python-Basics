0cb0010dae05120780ee501225413a4a1549cbec
Peter Tsai <hyptsai@gmail.com>
2017-11-09 22:20:40 +0200

    OS path utilities

--- 12.os_path_utils.py
+++ 12.os_path_utils.py
@@ -29,5 +29,6 @@ def main():
     td = datetime.datetime.now() - datetime.datetime.fromtimestamp(path.getmtime("textfile.txt"));
     print "It has been " + str(td) + " since the file was modified";
     print "Or, " + str(td.total_seconds()) + "seconds";
+	
 if __name__ == "__main__":
     main()
